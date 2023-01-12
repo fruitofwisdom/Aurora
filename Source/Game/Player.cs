d9ce2f349804411ed28907c2e9c1004ddc315423
Ward Childress <ward.childress@gmail.com>
2023-01-12 12:40:16 -0500

    Moved player verbs into their own file and improved input parsing.
    Moved player verbs into their own file to scale better for future growth. Improved input parsing by better looking for the verb, a preposition, and an object.

--- Source/Game/Player.cs
+++ Source/Game/Player.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections.Generic;
-using System.Windows;
 
 namespace Aurora
 {
-    internal class Player : GameObject
+    internal partial class Player : GameObject
     {
         private Connection LocalConnection;
 
@@ -41,30 +40,67 @@ namespace Aurora
             LocalConnection.SendMessage(message);
         }
 
-        private static string GetCommand(string input)
+		// Is a word one of the ten most commonly used prepositions?
+		private static bool IsPreposition(string word)
         {
-            string command = input.Split()[0].ToLower();
-            command = command.Trim();
-            return command;
+            List<string> prepositions = new()
+                { "of", "with", "at", "from", "into", "to", "in", "for", "on", "by" };
+            return prepositions.Contains(word);
         }
 
-        private static string GetArgument(string input)
+        private static string GetObjectFromInput(string[] input, int index)
         {
-            string argument = "";
-            string[] words = input.Split(' ');
-            for (int i = 1; i < words.Length; ++i)
+            string inputObject = "";
+            for (int i = index; i < input.Length; ++i)
             {
-                argument += words[i];
-                if (i != words.Length - 1)
+                inputObject += input[i];
+                if (i != input.Length - 1)
                 {
-                    argument += " ";
+                    inputObject += ' ';
                 }
             }
-            argument = argument.Trim();
-            return argument;
+            return inputObject;
         }
 
-        private string LookupShorthand(string input)
+        // Split an input into three possible parts: the verb, a preposition, and an object. For
+        // example: ("look", "at", "the fat baker") or ("drop", null, "manual")
+        private static (string, string, string) SplitInput(string input)
+        {
+            string inputVerb = null;
+            string inputPreposition = null;
+            string inputObject = null;
+
+            string[] words = input.ToLower().Split(' ');
+
+            if (words.Length > 0)
+            {
+				// The first word is always treated as the verb.
+				inputVerb = LookupShorthand(words[0]);
+            }
+            if (words.Length > 1)
+            {
+                // The next word may be a preposition.
+                if (IsPreposition(words[1]))
+                {
+                    inputPreposition = words[1];
+
+                    if (words.Length > 2)
+                    {
+                        // The rest of the words are the object.
+                        inputObject = GetObjectFromInput(words, 2);
+                    }
+				}
+                else if (words.Length > 1)
+				{
+					// The rest of the words are the object.
+					inputObject = GetObjectFromInput(words, 1);
+				}
+			}
+
+            return (inputVerb, inputPreposition, inputObject);
+        }
+
+        private static string LookupShorthand(string input)
         {
             string toReturn = input;
 
@@ -104,11 +140,12 @@ namespace Aurora
                 input = LastInput;
             }
 
-            string command = GetCommand(input);
-            command = LookupShorthand(command);
-            string argument = GetArgument(input);
+            (string, string, string) splitInput = SplitInput(input);
+            string inputVerb = splitInput.Item1;
+            string inputPreposition = splitInput.Item2;
+            string inputObject = splitInput.Item3;
 
-            switch (command)
+            switch (inputVerb)
             {
                 case "exit":
                 case "quit":
@@ -121,9 +158,9 @@ namespace Aurora
                     PrintHelp();
                     break;
                 case "look":
-                    if (argument.Split(' ')[0] == "at")
+                    if (inputObject != null)
                     {
-                        LookAt(GetArgument(argument));
+                        LookAt(inputObject);
                     }
                     else
                     {
@@ -137,26 +174,26 @@ namespace Aurora
                     PrintWho();
                     break;
                 case "say":
-                    Say(argument);
+                    Say(inputObject);
                     break;
                 case "emote":
-                    Emote(argument);
+                    Emote(inputObject);
                     break;
                 case "inventory":
                 case "inv":
                     PrintInventory();
                     break;
                 case "take":
-                    Take(argument);
+                    Take(inputObject);
                     break;
                 case "drop":
-                    Drop(argument);
+                    Drop(inputObject);
                     break;
                 case "shutdown":
                     Shutdown();
                     break;
                 default:
-                    TryExit(command);
+                    TryExit(inputVerb);
                     break;
             }
 
@@ -164,235 +201,6 @@ namespace Aurora
 
             // after each command, we need to tell the player about the state of the room
             PrintRoom();
-        }
-
-        public void PrintRoom()
-        {
-            LocalConnection.SendMessage("\r\n");
-            LocalConnection.SendMessage(ColorCodes.Color.Yellow, Game.Instance.GetRoomName(CurrentRoomId) + "\r\n");
-            if (DescriptionNeeded)
-            {
-                LocalConnection.SendMessage(Game.Instance.GetRoomDescription(CurrentRoomId) + "\r\n");
-                DescriptionNeeded = false;
-            }
-            string roomContents = Game.Instance.GetRoomContents(this);
-            if (roomContents != "")
-            {
-                LocalConnection.SendMessage(roomContents);
-            }
-            LocalConnection.SendMessage("> ");
-        }
-
-        private void PrintHelp()
-        {
-            LocalConnection.SendMessage("Type \"exit\" or \"quit\" to finish playing.\r\n");
-            LocalConnection.SendMessage("     \"help\" or \"?\" to see these instructions.\r\n");
-            LocalConnection.SendMessage("     \"look\" to look around at your surroundings.\r\n");
-            LocalConnection.SendMessage("     \"look at\" to look at something near you.\r\n");
-            LocalConnection.SendMessage("     \"north\", \"n\", \"south\", etc to move around the environment.\r\n");
-            LocalConnection.SendMessage("     \"exits\" to see obvious exits.\r\n");
-            LocalConnection.SendMessage("     \"who\" to list who else is playing.\r\n");
-            LocalConnection.SendMessage("     \"say\" to say something to everyone nearby.\r\n");
-            LocalConnection.SendMessage("     \"emote\" to express yourself.\r\n");
-			LocalConnection.SendMessage("     \"inventory\" or \"inv\" to list what you're carrying.\r\n");
-			LocalConnection.SendMessage("     \"take\" to pick something up.\r\n");
-			LocalConnection.SendMessage("     \"drop\" to drop something.\r\n");
-			LocalConnection.SendMessage("     \"!\" to repeat your last command.\r\n");
-            if (IsAdmin)
-            {
-				LocalConnection.SendMessage("     \"shutdown\" to shutdown the server. (admin)\r\n");
-			}
-		}
-
-        private void LookAt(string gameObjectName)
-        {
-			// Try looking in your inventory first.
-			bool wasInInventory = true;
-			GameObject gameObject = GetObjectFromInventory(gameObjectName);
-            if (gameObject == null)
-            {
-                // Then try looking in the world.
-				wasInInventory = false;
-				gameObject = Game.GetGameObject(gameObjectName, CurrentRoomId);
-            }
-
-            if (gameObject != null)
-            {
-                string message = "";
-                if (wasInInventory)
-                {
-                    message += "(in your inventory) ";
-                }
-                // If a description has a period at the end, treat it as its own sentence.
-                if (gameObject.Description[gameObject.Description.Length - 1] == '.')
-                {
-                    message += gameObject.Description + "\r\n";
-                }
-                // Otherwise, describe the object succinctly.
-                else
-                {
-                    message += "You see " + gameObject.Description + ".\r\n";
-                }
-				LocalConnection.SendMessage(message);
-			}
-            else
-            {
-                LocalConnection.SendMessage("You don't see a " + gameObjectName + " here.\r\n");
-            }
-		}
-
-        private void PrintExits()
-        {
-            List<(string, int, string)> exits = Game.Instance.GetRoomExits(CurrentRoomId);
-
-            if (exits.Count == 0)
-            {
-                LocalConnection.SendMessage("You see no obvious exits.\r\n");
-            }
-            else
-            {
-                LocalConnection.SendMessage("Obvious exits are:\r\n");
-                foreach ((string, int, string) exit in exits)
-                {
-                    string direction = char.ToUpper(exit.Item1[0]) + exit.Item1.Substring(1);
-                    LocalConnection.SendMessage("     " + direction + " leads to " + exit.Item3 + ".\r\n");
-                }
-            }
-        }
-
-        private void PrintWho()
-        {
-            if (Game.Instance.GetPlayerCount() == 1)
-            {
-                LocalConnection.SendMessage("There is 1 player currently:\r\n");
-            }
-            else
-            {
-                LocalConnection.SendMessage("There are " + Game.Instance.GetPlayerCount() + " players currently:\r\n");
-            }
-            foreach (string playerName in Game.Instance.GetPlayerNames())
-            {
-                if (Game.Instance.PlayerIsAdmin(playerName))
-                {
-					LocalConnection.SendMessage("     " + playerName + " (admin)\r\n");
-				}
-				else
-                {
-                    LocalConnection.SendMessage("     " + playerName + "\r\n");
-                }
-            }
-        }
-
-        private void Say(string argument)
-        {
-            LocalConnection.SendMessage("You say, \"" + argument + "\"\r\n");
-            Game.Instance.ReportPlayerSaid(this, argument);
-        }
-        private void Emote(string argument)
-        {
-            LocalConnection.SendMessage("You " + argument + ".\r\n");
-            Game.Instance.ReportPlayerEmoted(this, argument);
-        }
-
-        private GameObject GetObjectFromInventory(string gameObjectName)
-        {
-            GameObject toReturn = null;
-
-            foreach (GameObject gameObject in Inventory)
-            {
-                if (gameObject.Name.ToLower() == gameObjectName.ToLower())
-                {
-                    toReturn = gameObject;
-                }
-            }
-
-			return toReturn;
-        }
-
-        private void PrintInventory()
-        {
-            if (Inventory.Count == 0)
-            {
-				LocalConnection.SendMessage("You're not carrying anything.\r\n");
-			}
-            else
-            {
-				LocalConnection.SendMessage("You are carrying:\r\n");
-                foreach (GameObject gameObject in Inventory)
-                {
-                    LocalConnection.SendMessage("     " + gameObject.CapitalizeName() + "\r\n");
-				}
-			}
-		}
-
-        private void Take(string argument)
-        {
-            GameObject gameObject = Game.Instance.TryTake(this, argument);
-            if (gameObject != null)
-            {
-                LocalConnection.SendMessage("You take " + gameObject.Description + ".\r\n");
-                Inventory.Add(gameObject);
-            }
-            else
-            {
-                LocalConnection.SendMessage("You can't take that!\r\n");
-            }
-        }
-
-        private void Drop(string argument)
-        {
-            GameObject gameObject = GetObjectFromInventory(argument);
-			if (gameObject != null)
-			{
-                Game.Instance.TryDrop(this, gameObject);
-				LocalConnection.SendMessage("You drop " + gameObject.Description + ".\r\n");
-				Inventory.Remove(gameObject);
-			}
-			else
-			{
-				LocalConnection.SendMessage("You don't have that!\r\n");
-			}
-		}
-
-        private void Shutdown()
-        {
-            if (IsAdmin)
-            {
-                Server.Instance.ShutdownAsync();
-            }
-            else
-            {
-				LocalConnection.SendMessage("Only admins may shutdown the server!\r\n");
-			}
-		}
-
-		private void TryExit(string command)
-        {
-            bool didExit = false;
-
-            int? newRoomId = Game.Instance.RoomContainsExit(CurrentRoomId, command);
-            if (newRoomId != null)
-            {
-                if (Game.Instance.RoomExists((int)newRoomId))
-                {
-                    Game.Instance.ReportPlayerMoved(this, CurrentRoomId, (int)newRoomId);
-                    CurrentRoomId = (int)newRoomId;
-					didExit = true;
-                }
-                else
-                {
-                    ServerInfo.Instance.Report(
-                        ColorCodes.Color.Red,
-                        "[Player \"" + Name + "\"] Room with room_id " + newRoomId + " wasn't found!\n");
-                }
-            }
-
-            if (!didExit)
-            {
-                LocalConnection.SendMessage("You can't \"" + command + "\" here!\r\n");
-            }
-
-            DescriptionNeeded = didExit;
         }
     }
 }
---
+++ Source/Game/PlayerVerbs.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections.Generic;
+
+namespace Aurora
+{
+	internal partial class Player : GameObject
+	{
+		public void PrintRoom()
+		{
+			LocalConnection.SendMessage("\r\n");
+			LocalConnection.SendMessage(ColorCodes.Color.Yellow, Game.Instance.GetRoomName(CurrentRoomId) + "\r\n");
+			if (DescriptionNeeded)
+			{
+				LocalConnection.SendMessage(Game.Instance.GetRoomDescription(CurrentRoomId) + "\r\n");
+				DescriptionNeeded = false;
+			}
+			string roomContents = Game.Instance.GetRoomContents(this);
+			if (roomContents != "")
+			{
+				LocalConnection.SendMessage(roomContents);
+			}
+			LocalConnection.SendMessage("> ");
+		}
+
+		private void PrintHelp()
+		{
+			LocalConnection.SendMessage("Type \"exit\" or \"quit\" to finish playing.\r\n");
+			LocalConnection.SendMessage("     \"help\" or \"?\" to see these instructions.\r\n");
+			LocalConnection.SendMessage("     \"look\" to look around at your surroundings.\r\n");
+			LocalConnection.SendMessage("     \"look at\" to look at something near you.\r\n");
+			LocalConnection.SendMessage("     \"north\", \"n\", \"south\", etc to move around the environment.\r\n");
+			LocalConnection.SendMessage("     \"exits\" to see obvious exits.\r\n");
+			LocalConnection.SendMessage("     \"who\" to list who else is playing.\r\n");
+			LocalConnection.SendMessage("     \"say\" to say something to everyone nearby.\r\n");
+			LocalConnection.SendMessage("     \"emote\" to express yourself.\r\n");
+			LocalConnection.SendMessage("     \"inventory\" or \"inv\" to list what you're carrying.\r\n");
+			LocalConnection.SendMessage("     \"take\" to pick something up.\r\n");
+			LocalConnection.SendMessage("     \"drop\" to drop something.\r\n");
+			LocalConnection.SendMessage("     \"!\" to repeat your last command.\r\n");
+			if (IsAdmin)
+			{
+				LocalConnection.SendMessage("     \"shutdown\" to shutdown the server. (admin)\r\n");
+			}
+		}
+
+		private void LookAt(string gameObjectName)
+		{
+			// Try looking in your inventory first.
+			bool wasInInventory = true;
+			GameObject gameObject = GetObjectFromInventory(gameObjectName);
+			if (gameObject == null)
+			{
+				// Then try looking in the world.
+				wasInInventory = false;
+				gameObject = Game.GetGameObject(gameObjectName, CurrentRoomId);
+			}
+
+			if (gameObject != null)
+			{
+				string message = "";
+				if (wasInInventory)
+				{
+					message += "(in your inventory) ";
+				}
+				// If a description has a period at the end, treat it as its own sentence.
+				if (gameObject.Description[gameObject.Description.Length - 1] == '.')
+				{
+					message += gameObject.Description + "\r\n";
+				}
+				// Otherwise, describe the object succinctly.
+				else
+				{
+					message += "You see " + gameObject.Description + ".\r\n";
+				}
+				LocalConnection.SendMessage(message);
+			}
+			else
+			{
+				LocalConnection.SendMessage("You don't see a " + gameObjectName + " here.\r\n");
+			}
+		}
+
+		private void PrintExits()
+		{
+			List<(string, int, string)> exits = Game.Instance.GetRoomExits(CurrentRoomId);
+
+			if (exits.Count == 0)
+			{
+				LocalConnection.SendMessage("You see no obvious exits.\r\n");
+			}
+			else
+			{
+				LocalConnection.SendMessage("Obvious exits are:\r\n");
+				foreach ((string, int, string) exit in exits)
+				{
+					string direction = char.ToUpper(exit.Item1[0]) + exit.Item1.Substring(1);
+					LocalConnection.SendMessage("     " + direction + " leads to " + exit.Item3 + ".\r\n");
+				}
+			}
+		}
+
+		private void PrintWho()
+		{
+			if (Game.Instance.GetPlayerCount() == 1)
+			{
+				LocalConnection.SendMessage("There is 1 player currently:\r\n");
+			}
+			else
+			{
+				LocalConnection.SendMessage("There are " + Game.Instance.GetPlayerCount() + " players currently:\r\n");
+			}
+			foreach (string playerName in Game.Instance.GetPlayerNames())
+			{
+				if (Game.Instance.PlayerIsAdmin(playerName))
+				{
+					LocalConnection.SendMessage("     " + playerName + " (admin)\r\n");
+				}
+				else
+				{
+					LocalConnection.SendMessage("     " + playerName + "\r\n");
+				}
+			}
+		}
+
+		private void Say(string argument)
+		{
+			LocalConnection.SendMessage("You say, \"" + argument + "\"\r\n");
+			Game.Instance.ReportPlayerSaid(this, argument);
+		}
+		private void Emote(string argument)
+		{
+			LocalConnection.SendMessage("You " + argument + ".\r\n");
+			Game.Instance.ReportPlayerEmoted(this, argument);
+		}
+
+		private GameObject GetObjectFromInventory(string gameObjectName)
+		{
+			GameObject toReturn = null;
+
+			foreach (GameObject gameObject in Inventory)
+			{
+				if (gameObject.Name.ToLower() == gameObjectName.ToLower())
+				{
+					toReturn = gameObject;
+				}
+			}
+
+			return toReturn;
+		}
+
+		private void PrintInventory()
+		{
+			if (Inventory.Count == 0)
+			{
+				LocalConnection.SendMessage("You're not carrying anything.\r\n");
+			}
+			else
+			{
+				LocalConnection.SendMessage("You are carrying:\r\n");
+				foreach (GameObject gameObject in Inventory)
+				{
+					LocalConnection.SendMessage("     " + gameObject.CapitalizeName() + "\r\n");
+				}
+			}
+		}
+
+		private void Take(string argument)
+		{
+			GameObject gameObject = Game.Instance.TryTake(this, argument);
+			if (gameObject != null)
+			{
+				LocalConnection.SendMessage("You take " + gameObject.Description + ".\r\n");
+				Inventory.Add(gameObject);
+			}
+			else
+			{
+				LocalConnection.SendMessage("You can't take that!\r\n");
+			}
+		}
+
+		private void Drop(string argument)
+		{
+			GameObject gameObject = GetObjectFromInventory(argument);
+			if (gameObject != null)
+			{
+				Game.Instance.TryDrop(this, gameObject);
+				LocalConnection.SendMessage("You drop " + gameObject.Description + ".\r\n");
+				Inventory.Remove(gameObject);
+			}
+			else
+			{
+				LocalConnection.SendMessage("You don't have that!\r\n");
+			}
+		}
+
+		private void Shutdown()
+		{
+			if (IsAdmin)
+			{
+				Server.Instance.ShutdownAsync();
+			}
+			else
+			{
+				LocalConnection.SendMessage("Only admins may shutdown the server!\r\n");
+			}
+		}
+
+		private void TryExit(string command)
+		{
+			bool didExit = false;
+
+			int? newRoomId = Game.Instance.RoomContainsExit(CurrentRoomId, command);
+			if (newRoomId != null)
+			{
+				if (Game.Instance.RoomExists((int)newRoomId))
+				{
+					Game.Instance.ReportPlayerMoved(this, CurrentRoomId, (int)newRoomId);
+					CurrentRoomId = (int)newRoomId;
+					didExit = true;
+				}
+				else
+				{
+					ServerInfo.Instance.Report(
+						ColorCodes.Color.Red,
+						"[Player \"" + Name + "\"] Room with room_id " + newRoomId + " wasn't found!\n");
+				}
+			}
+
+			if (!didExit)
+			{
+				LocalConnection.SendMessage("You can't \"" + command + "\" here!\r\n");
+			}
+
+			DescriptionNeeded = didExit;
+		}
+	}
+}
