90ac9a24a8af8d67431573e5761dac99a367a00e
Ward Childress <ward.childress@gmail.com>
2023-05-16 15:15:14 -0400

    Fleshing out the world with better descriptions.
    Fleshing out the world with better descriptions and new NPC objects.

--- Source/Game/NPC.cs
+++ Source/Game/NPC.cs
@@ -1,7 +1,14 @@
 ﻿namespace Aurora
 {
+	// NPCs are simple mobiles who can have built-in responses to being read (such as a sign or book)
+	// or talked to (such as a person). Other responses can be hard-coded here.
 	internal class NPC : Mobile
 	{
+		#region JSON-serialized public fields.
+		public string Read { get; set; }
+		public string Talk { get; set; }
+		#endregion
+
 		protected override void Do(string action)
 		{
 			if (action == "Say Hello")
